7c55cfb559115cbae7d53aeb86403afd07a492a1
Jeremías <jrmsdev@gmail.com>
2019-03-21 20:51:45 -0300

    install default config

--- install.py
+++ install.py
@@ -11,6 +11,9 @@ if __name__ == '__main__':
 	os.system('go install ./bin/sadm')
 	print('%s/bin/sadm installed' % prefix)
 
+	os.system('mkdir -vp %s/etc/sadm.d' % prefix)
+	os.system('cp -va etc/sadm.json %s/etc' % prefix)
+
 	os.system('mkdir -vp %s/lib/sadm' % prefix)
 	os.system('cp -va lib %s/lib/sadm' % prefix)
 

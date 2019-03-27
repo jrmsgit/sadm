c88f5f4781fda00f51d4de4cf44fe72df2fa8682
Jeremías <jrmsdev@gmail.com>
2019-03-27 03:43:16 -0300

    parse ldd output

--- internal/utils/ldd/list.go
+++ internal/utils/ldd/list.go
@@ -4,6 +4,7 @@
 package ldd
 
 import (
+	"path/filepath"
 	"strings"
 
 	"github.com/jrmsdev/sadm/internal/env/args"
@@ -22,18 +23,41 @@ func List(opt *args.Args, filename string) (*Info, error) {
 		log.Debug("%s", err)
 		return nil, err
 	} else {
-		if err := parse(l.Files, string(out)); err != nil {
+		if err := parse(&l.Files, string(out)); err != nil {
 			return nil, err
 		}
 	}
 	return l, nil
 }
 
-func parse(dst []string, src string) error {
+func parse(dst *[]string, src string) error {
 	log.Debug("parse")
 	for _, line := range strings.Split(src, "\n") {
 		line = strings.TrimSpace(line)
-		log.Print(line)
+		if line == "" {
+			continue
+		}
+		//~ log.Print(line)
+		items := strings.Split(line, " => ")
+		ilen := len(items)
+		tok0 := strings.TrimSpace(items[0])
+		if ilen == 1 {
+			fn := strings.Split(tok0, " ")[0]
+			checkFile(dst, fn)
+		} else {
+			tok1 := strings.TrimSpace(items[1])
+			fn := strings.Split(tok1, " ")[0]
+			checkFile(dst, fn)
+		}
 	}
 	return nil
+}
+
+func checkFile(dst *[]string, fn string) {
+	log.Debug("check file %s", fn)
+	if !filepath.IsAbs(fn) {
+		log.Warnf("ldd ignore %s", fn)
+		return
+	}
+	*dst = append(*dst, fn)
 }

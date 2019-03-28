9f085f83f79ec39d9d21cc0c32d76cca49b1c57a
Jeremías <jrmsdev@gmail.com>
2019-03-28 16:35:34 -0300

    test coverage

--- test.sh
+++ test.sh
@@ -1,2 +1,15 @@
 #!/bin/sh -eu
-go test ./...
+verbose=''
+if test "${1:-'none'}" = '-v'; then
+  shift
+  verbose='-v'
+fi
+coverage=''
+if test "${1:-'none'}" = '--coverage'; then
+  shift
+  coverage='-coverprofile coverage.out'
+fi
+go test $verbose $coverage ./...
+if test "X${coverage}" != 'X'; then
+  go tool cover -html coverage.out -o coverage.html
+fi

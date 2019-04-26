82215053b45b9e1ea8869c0f7d16f45cb530b5cf
Jeremías <jrmsdev@gmail.com>
2019-04-26 13:36:42 -0300

    git clone depth 3

--- vendor/deps.sh
+++ vendor/deps.sh
@@ -3,5 +3,5 @@ deps='github.com/jrmsdev/gojc'
 for repo in $(echo ${deps}); do
 	echo ${repo}
 	mkdir -vp ./vendor/${repo}
-	git clone --depth 5 https://${repo}.git ./vendor/${repo}
+	git clone --depth 3 https://${repo}.git ./vendor/${repo}
 done
